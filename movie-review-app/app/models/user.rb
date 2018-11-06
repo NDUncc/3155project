3f0c0df3ec6e59b138a7c2820ed45e6aed2aed40
NDUncc <42954359+NDUncc@users.noreply.github.com>
2018-11-06 10:34:18 -0500

    Update user.rb

--- movie-review-app/app/models/user.rb
+++ movie-review-app/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :reviews
-    validates :name, presence: true, length: { minimum: 5 }
-    validates :password, presence: true, length: { minimum: 5 }
+    validates :name, presence: true, length: { minimum: 6 }
+    validates :password, presence: true, length: { minimum: 6 }
 end
+
